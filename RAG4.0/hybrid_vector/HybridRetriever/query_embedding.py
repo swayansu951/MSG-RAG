62b2bdc39819b8ceebefba789c41f73fa3623f5a
swayansu951 <swayansusahu951@gmail.com>
2026-03-22 18:00:08 +0530

    Delete RAG4.0/hybrid_vector directory

--- RAG4.0/hybrid_vector/HybridRetriever/query_embedding.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('all-mpnet-base-v2')
-
-def embed_query(query):
-    return model.encode([query]).astype('float32')
